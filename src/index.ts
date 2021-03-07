194624358ecd60ea0900c633f5c60bb2d338585f
Dariusz Filipiak <dariusz.filipiak@igt.com>
2021-03-07 21:21:55 +0100

    asd

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+//#region @backend
+export * from './node-cli-tester.backend';
+export { run } from './start.backend';
+//#endregion
+
+//#region @notForNpm
+export function helloWorld() {
+
+}``
+//#endregion
