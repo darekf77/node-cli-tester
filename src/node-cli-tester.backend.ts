c34da4b9bb42cd1eca2295c119534ce058628eb9
Dariusz Filipiak <dariusz.filipiak@igt.com>
2021-03-14 04:22:27 +0100

    update

--- src/node-cli-tester.backend.ts
+++ src/node-cli-tester.backend.ts
@@ -1,3 +1,4 @@
+import * as _ from 'lodash';
 import * as path from 'path';
 import { Helpers } from 'tnp-helpers';
 import { config } from 'tnp-config';
@@ -7,6 +8,7 @@ export class NodeCliTester {
   //#region singleton
   private static _instances = {};
   public static classFn = NodeCliTester;
+  protected NAME_FOR_CLI_TESTS_FOLDER = 'cli-tests';
 
   protected constructor(
     protected readonly cwd = process.cwd()
@@ -20,18 +22,45 @@ export class NodeCliTester {
   }
   public static Instance(cwd = process.cwd()) {
     if (!NodeCliTester._instances[cwd]) {
-      NodeCliTester._instances[cwd] = new (NodeCliTester.classFn)(cwd);
+      NodeCliTester._instances[cwd] = new (this.classFn)(cwd);
     }
     return NodeCliTester._instances[cwd] as NodeCliTester;
   }
   //#endregion
 
-  createTest(testName: string) {
-    console.log(`Create test from node-cli-tester`);
+  async createTest(testName: string) {
+    Helpers.log(`Create test from node-cli-tester`);
+    const folderName = _.kebabCase(testName);
+    const testFolderPath = path.join(this.cwd, this.NAME_FOR_CLI_TESTS_FOLDER, folderName);
+    Helpers.mkdirp(testFolderPath);
+    const testPackageJsonPath = path.join(
+      this.cwd,
+      this.NAME_FOR_CLI_TESTS_FOLDER,
+      folderName,
+      config.file.package_json,
+    );
+    Helpers.writeFile(testPackageJsonPath, `{
+      // generated from basename
+      "name": "${folderName}",
+      "tnp": {
+        "type": "cli-test"
+      },
+      "version": "0.0.0",
+      // git ignroe all files except this
+      "files-list": [
+        "nes-ui/node_modules/es-common/src/es-common-module.ts",
+      ]
+    }`);
   }
 
-  addFileToTest(testName: string, filePath: string) {
+  async createTestAndAddFile(testName: string, filePath: string) {
+    await this.createTest(testName);
+    await this.addFileToTest(testName, filePath)
+  }
 
+
+  addFileToTest(testName: string, filePath: string) {
+    // const folderName
   }
 
   getAllTestsNames() {
