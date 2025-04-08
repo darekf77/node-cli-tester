a3982f76fc9643cee8450d9536f6dc04ebf70ae5
Dariusz Filipiak <darekf77@gmail.com>
2025-04-08 20:26:45 +0200

    release: v18.0.20

---
+++ env.ts
@@ -0,0 +1,9 @@
+import type { EnvOptions } from 'tnp/src';
+
+const env: Partial<EnvOptions> = {
+  website: {
+    domain: 'node-cli-tester.example.domain.com',
+    title: 'Node Cli Tester',
+  },
+};
+export default env;
